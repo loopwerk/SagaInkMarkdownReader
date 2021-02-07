2329b3fc1603b17e29fbf8aaf1234e39276a4481
Kevin Renskers <kevin@loopwerk.io>
2021-02-07 23:10:11 +0100

    Uses newest version of Saga

--- Sources/SagaInkMarkdownReader/SagaInkMarkdownReader.swift
+++ Sources/SagaInkMarkdownReader/SagaInkMarkdownReader.swift
@@ -8,8 +8,8 @@ import Slugify
 
 public extension Reader {
   static func inkMarkdownReader(pageProcessor: ((Page<M>) -> Void)? = nil) -> Self {
-    Reader(supportedExtensions: ["md", "markdown"], convert: { path, relativePath in
-      let contents: String = try path.read()
+    Reader(supportedExtensions: ["md", "markdown"], convert: { absoluteSource, relativeSource, relativeDestination in
+      let contents: String = try absoluteSource.read()
 
       // First we parse the markdown file, and use the Splash syntax highlighter
       var markdownParser = MarkdownParser()
@@ -18,20 +18,21 @@ public extension Reader {
 
       // Then we try to decode the embedded metadata within the markdown (which otherwise is just a [String: String] dict)
       let decoder = makeMetadataDecoder(for: markdown.metadata)
-      let date = try resolvePublishingDate(from: path, decoder: decoder)
+      let date = try resolvePublishingDate(from: absoluteSource, decoder: decoder)
       let metadata = try M.init(from: decoder)
-      let template = try decoder.decodeIfPresent("template", as: String.self)
+      let template = try decoder.decodeIfPresent("template", as: Path.self)
 
       // Create the Page
       let page = Page(
-        relativeSource: relativePath,
-        title: markdown.title ?? path.lastComponentWithoutExtension,
+        relativeSource: relativeSource,
+        relativeDestination: relativeDestination,
+        title: markdown.title ?? relativeSource.lastComponentWithoutExtension,
         rawContent: contents,
         body: markdown.html,
         date: date,
-        lastModified: path.modificationDate ?? Date(),
+        lastModified: absoluteSource.modificationDate ?? Date(),
         metadata: metadata,
-        template: template != nil ? Path(template!) : nil
+        template: template
       )
 
       // Run the processor, if any, to modify the Page
